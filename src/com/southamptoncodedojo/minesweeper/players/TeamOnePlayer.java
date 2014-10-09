21ce195f9905bac632346ae8ada9b87b0913a8fe
Laura Dragan <aprilush@gmail.com>
2014-10-09 20:01:29 +0100

    team 1 player

--- src/com/southamptoncodedojo/minesweeper/players/TeamOnePlayer.java
+++ src/com/southamptoncodedojo/minesweeper/players/TeamOnePlayer.java
@@ -1,11 +1,22 @@
 package com.southamptoncodedojo.minesweeper.players;
 
+import java.util.Random;
+
 import com.southamptoncodedojo.minesweeper.Coordinate;
+import com.southamptoncodedojo.minesweeper.MapInstance;
 import com.southamptoncodedojo.minesweeper.MapState;
 import com.southamptoncodedojo.minesweeper.Player;
+import com.southamptoncodedojo.minesweeper.exceptions.InvalidCoordinateException;
 
 public class TeamOnePlayer extends Player {
 
+	private int remainingMines;
+	private int[][] scores;
+	
+	public TeamOnePlayer() {
+		remainingMines = -1;
+	}
+	
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
@@ -14,6 +25,23 @@ public class TeamOnePlayer extends Player {
 
 	@Override
 	public Coordinate takeTurn(MapState mapState) {
+		if (remainingMines < 1) {
+			// first run!
+			remainingMines = mapState.getNumberOfMines();
+			System.out.println("number of mines "+remainingMines);
+			return new Coordinate(0, 0);
+		} else {
+			computeScores();
+			Coordinate next = pickBestNext();
+			return next;
+		}
+	}
+	
+	private void computeScores()  {
+		
+	}
+	
+	private Coordinate pickBestNext() {
 		
 	}
 

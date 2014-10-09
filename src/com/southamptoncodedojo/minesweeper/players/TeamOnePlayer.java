48f89c6842894680ccead566235c7864cc43c9a7
Laura Dragan <aprilush@gmail.com>
2014-10-09 19:35:07 +0100

    team 1 player

---
+++ src/com/southamptoncodedojo/minesweeper/players/TeamOnePlayer.java
@@ -0,0 +1,20 @@
+package com.southamptoncodedojo.minesweeper.players;
+
+import com.southamptoncodedojo.minesweeper.Coordinate;
+import com.southamptoncodedojo.minesweeper.MapState;
+import com.southamptoncodedojo.minesweeper.Player;
+
+public class TeamOnePlayer extends Player {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Team One";
+	}
+
+	@Override
+	public Coordinate takeTurn(MapState mapState) {
+		
+	}
+
+}
